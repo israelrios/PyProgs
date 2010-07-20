4aa69850efc9bb35c06e5e135c61d8629f925cb8
Israel Rios <isrrios@gmail.com>
2010-07-20 12:26:19 -0300

    Removing line numbers from po files and optmizing scripts.

--- checkmail/po/refresh_po.sh
+++ checkmail/po/refresh_po.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
 pot=monitors.pot
+newpot=/tmp/monitors.pot.new
 
-xgettext -s --no-wrap --output=$pot ../*.py
+xgettext --no-location -s --no-wrap --output=$newpot ../*.py
+
+#check for changes
+if ! diff --changed-group-format="%<" --new-group-format=%L --old-group-format=%L \
+	--unchanged-group-format=  $pot $newpot | grep -qve '^"POT-Creation-Date:'; then
+	rm $newpot
+	echo "no changes in ${pot}."
+	exit
+fi
+
+mv $newpot $pot
 
 pofiles=`ls | grep -e \.po$`
 
 for f in $pofiles; do
-	msgmerge -v -s --no-wrap -U "${f}" $pot
+	msgmerge --no-location -v -s --no-wrap -U "${f}" $pot
 done
