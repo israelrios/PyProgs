21792ff3f02e91d82df13a0e44469f2c3992f987
Israel Rios <isrrios@gmail.com>
2010-08-30 09:41:58 -0300

    New version.

--- checkmail/monitors.py
+++ checkmail/monitors.py
@@ -16,7 +16,7 @@ import tempfile
 import fcntl
 import gettext
 
-version = "1.7.1"
+version = "1.7.2"
 
 if 'http_proxy' in os.environ:
     del os.environ['http_proxy'] #não utiliza proxy para acessar as páginas
