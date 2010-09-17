e75fa4e6bae7a2e98fc5d692895e5c9571990c8c
Israel Rios <isrrios@gmail.com>
2010-09-17 10:00:14 -0300

    Split MultipartPostHandler from iexpresso.

--- checkmail/iexpresso.py
+++ checkmail/iexpresso.py
@@ -5,7 +5,7 @@
 
 import imap4utf7 # pro codec imap4-utf-7
 
-from monutil import decode_header
+from monutil import decode_header, MultipartPostHandler
 
 import sys
 import urllib
@@ -30,9 +30,6 @@ import email.header
 from email.generator import Generator as EmailGenerator
 
 import imaplib
-
-import mimetypes
-import mimetools
 import hashlib
 
 #diretório das configurações e do log
@@ -102,68 +99,6 @@ class NamedStringIO(pyStringIO):
     def __init__(self, name, buffer = None):
         self.name = name
         pyStringIO.__init__(self, buffer)
-
-# Controls how sequences are encoded. If true, elements may be given multiple values by
-#  assigning a sequence.
-doseq = 1
-
-##########################################################
-# Peguei na NET esta classe para codificar os campos
-# do formulário no formato multipart/form-data
-class MultipartPostHandler(urllib2.BaseHandler):
-    handler_order = urllib2.HTTPHandler.handler_order - 10 # needs to run first
-
-    def http_request(self, request):
-        data = request.get_data()
-        if data is not None and type(data) != str:
-            v_files = []
-            v_vars = []
-            try:
-                 for(key, value) in data.items():
-                     if hasattr(value, 'read'):
-                         v_files.append((key, value))
-                     else:
-                         v_vars.append((key, value))
-            except TypeError, e:
-                raise TypeError("not a valid non-string sequence or mapping object: " + str(e))
-
-            if len(v_files) == 0:
-                data = urllib.urlencode(v_vars, doseq)
-            else:
-                boundary, data = self.multipart_encode(v_vars, v_files)
-                contenttype = 'multipart/form-data; boundary=%s' % boundary
-                if request.has_header('Content-Type') and request.get_header('Content-Type').find('multipart/form-data') != 0:
-                    log( "Replacing %s with %s" % (request.get_header('content-type'), 'multipart/form-data') )
-                request.add_unredirected_header('Content-Type', contenttype)
-
-            request.add_data(data)
-        return request
-
-    def multipart_encode(self, vars, files, boundary = None, buffer = None):
-        if boundary is None:
-            boundary = mimetools.choose_boundary()
-        if buffer is None:
-            buffer = ''
-        for(key, value) in vars:
-            buffer += '--%s\r\n' % boundary
-            buffer += 'Content-Disposition: form-data; name="%s"' % key
-            buffer += '\r\n\r\n' + str(value) + '\r\n'
-        for(key, fd) in files:
-            fd.seek(0, os.SEEK_END)
-            file_size = fd.tell()
-            filename = os.path.basename(fd.name)
-            contenttype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-            buffer += '--%s\r\n' % boundary
-            buffer += 'Content-Disposition: form-data; name="%s"; filename="%s"\r\n' % (key, filename)
-            buffer += 'Content-Type: %s\r\n' % contenttype
-            buffer += 'Content-Length: %s\r\n' % file_size
-            fd.seek(0, os.SEEK_SET)
-            buffer += '\r\n' + fd.read() + '\r\n'
-        buffer += '--%s--\r\n\r\n' % boundary
-        return boundary, buffer
-
-    https_request = http_request
-# end
 
 
 # expresso msg fields:
--- checkmail/monutil.py
+++ checkmail/monutil.py
@@ -8,6 +8,10 @@ import re
 import email.header
 import os
 import subprocess
+import urllib
+import urllib2
+import mimetypes
+import mimetools
 
 #Classe para extrair o texto do HTML
 class HtmlTextParser(HTMLParser):
@@ -57,3 +61,69 @@ def execute(cmd):
         os._exit(os.EX_OK)
     else:
         os.waitpid(pid, 0)
+
+
+# Controls how sequences are encoded. If true, elements may be given multiple values by
+#  assigning a sequence.
+doseq = 1
+
+##########################################################
+# Peguei na NET esta classe para codificar os campos
+# do formulário no formato multipart/form-data
+class MultipartPostHandler(urllib2.BaseHandler):
+    handler_order = urllib2.HTTPHandler.handler_order - 10 # needs to run first
+
+    def http_request(self, request):
+        data = request.get_data()
+        if data is not None and type(data) != str:
+            v_files = []
+            v_vars = []
+            try:
+                 for(key, value) in data.items():
+                     if hasattr(value, 'read'):
+                         v_files.append((key, value))
+                     else:
+                         v_vars.append((key, value))
+            except TypeError, e:
+                raise TypeError("not a valid non-string sequence or mapping object: " + str(e))
+
+            if len(v_files) == 0:
+                data = urllib.urlencode(v_vars, doseq)
+            else:
+                boundary, data = self.multipart_encode(v_vars, v_files)
+                contenttype = 'multipart/form-data; boundary=%s' % boundary
+                if request.has_header('Content-Type') and request.get_header('Content-Type').find('multipart/form-data') != 0:
+                    log( "Replacing %s with %s" % (request.get_header('content-type'), 'multipart/form-data') )
+                request.add_unredirected_header('Content-Type', contenttype)
+
+            request.add_data(data)
+        return request
+
+    def multipart_encode(self, vars, files, boundary = None, buffer = None):
+        if boundary is None:
+            boundary = mimetools.choose_boundary()
+        if buffer is None:
+            buffer = ''
+        for(key, value) in vars:
+            buffer += '--%s\r\n' % boundary
+            buffer += 'Content-Disposition: form-data; name="%s"' % key
+            buffer += '\r\n\r\n' + str(value) + '\r\n'
+        for(key, fd) in files:
+            fd.seek(0, os.SEEK_END)
+            file_size = fd.tell()
+            if hasattr(fd, 'name'):
+                filename = os.path.basename(fd.name)
+            else:
+                filename = 'inputfile'
+            contenttype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+            buffer += '--%s\r\n' % boundary
+            buffer += 'Content-Disposition: form-data; name="%s"; filename="%s"\r\n' % (key, filename)
+            buffer += 'Content-Type: %s\r\n' % contenttype
+            buffer += 'Content-Length: %s\r\n' % file_size
+            fd.seek(0, os.SEEK_SET)
+            buffer += '\r\n' + fd.read() + '\r\n'
+        buffer += '--%s--\r\n\r\n' % boundary
+        return boundary, buffer
+
+    https_request = http_request
+# end
