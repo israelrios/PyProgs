355b0e50bd6c027098dc2405e4046a72ac4694e4
Israel Rios <isrrios@gmail.com>
2012-01-10 16:48:04 -0200

    Reestructuring the service to run uninterruptedly. Day by day. Removing the use of Timer's. Better code readability.

--- checkmail/siscop.py
+++ checkmail/siscop.py
@@ -10,13 +10,13 @@ import urllib
 import urllib2
 import cookielib
 import datetime
-from threading import Timer
 import dbus
 import commands
 
 SEC_HOUR = 60*60 # 1 hora em segundos
 
-SEC_MAX_PERIOD = SEC_HOUR * 5 - (60 * 2) # 4hs e 58mins
+SEC_MAX_PERIOD = SEC_HOUR * 5 # 5hs
+SEC_ALERT_PERIOD = SEC_MAX_PERIOD - (60 * 2) #4hs e 58mins
 
 class SisCopTrayIcon(TrayIcon):
     def onActivate(self, event):
@@ -29,7 +29,7 @@ class SisCopTrayIcon(TrayIcon):
         else:
             iconname = 'siscop_waiting.png'
             tip = u'Aguarde para registrar o ponto ' + \
-                self.service.horaRetorno.strftime('(%H:%M)')
+                self.service.timeReturn.strftime('(%H:%M)')
         iconname = os.path.join(curdir, iconname)
         self.set_from_file(iconname)
         self.set_tooltip(tip)
@@ -38,9 +38,6 @@ class SisCopTrayIcon(TrayIcon):
 
 class SisCopService(Service):
     urlSisCop = 'http://siscop.portalcorporativo.serpro/cpf_senha.asp'
-    timer = None
-    alertExit1Period = True
-    timerExit1Period = None
     def __init__(self, app, user, passwd):
         Service.__init__(self, app, user, passwd)
         # Os campos do formulário
@@ -52,60 +49,40 @@ class SisCopService(Service):
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies))
         #Atualiza a cada 55min
         self.refreshMinutes = 55
+        self.lastPageId = None
     
     def createTrayIcon(self):
         return SisCopTrayIcon(self)
-
-    def releaseTimerExit1Period(self):
-        if self.timerExit1Period != None:
-            self.timerExit1Period.cancel()
-            self.timerExit1Period = None
-
-    def onQuit(self):
-        Service.onQuit(self)
-        if self.timer != None:
-            self.timer.cancel()
-        self.releaseTimerExit1Period()
 
     def runService(self, timered=True):
         #o valor de refreshMinutos pode ser alterado em self.check()
         self.refreshMinutes = 55 # não tem necessidade de estressar o servidor
         self.setIcon(self.check())
 
-    def showPage(self):
-        #abre o browser com a página
-        procs = commands.getoutput('/bin/ps xo comm').split('\n')
-        if 'chrome' in procs:
-            execute(["google-chrome", self.urlSisCop])
-            execute(["wmctrl", "-a", "Chrome"])
-        else:
-            execute(["firefox", self.urlSisCop])
-            execute(["wmctrl", "-a", "Firefox"])
+    def showPage(self, pageId):
+        """ Mostra a página do SisCop se o pageId for diferente do último. """
+        if pageId != self.lastPageId:
+            self.lastPageId = pageId
+            #abre o browser com a página
+            procs = commands.getoutput('/bin/ps xo comm').split('\n')
+            if 'chrome' in procs:
+                execute(["google-chrome", self.urlSisCop])
+                execute(["wmctrl", "-a", "Chrome"])
+            else:
+                execute(["firefox", self.urlSisCop])
+                execute(["wmctrl", "-a", "Firefox"])
 
-    def onTimer(self):
-        mustShowPage = self.timer != None
-        #self.setIcon(not self.timer != None)
-        self.timer = None
-        
-        if not mustShowPage:
-            #verifica se o usuário está na máquina
-            bus = dbus.SessionBus()
-            ssaver = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
-            mustShowPage = not ssaver.GetActive()
-            try:
-              ssaver.SimulateUserActivity() # faz aparecer a tela de login caso o screensaver esteja ativado
-            except:
-              pass # costuma lançar um erro DBusException: org.freedesktop.DBus.Error.NoReply
-        
-        if mustShowPage:
-            self.showPage()
+        #verifica se o usuário está na máquina
+        bus = dbus.SessionBus()
+        ssaver = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
+        mustShowPage = not ssaver.GetActive()
+        try:
+            ssaver.SimulateUserActivity() # faz aparecer a tela de login caso o screensaver esteja ativado
+        except:
+            pass # costuma lançar um erro DBusException: org.freedesktop.DBus.Error.NoReply
 
-    ###########################################################
-    # Extrai o texto do HTML e separa a informação do ponto
-    # Formato:
-    #   1º Período
-    #   Saída- 11:58
-    def check(self):
+    def getPageText(self):
+        """ Faz o login no SISCOP. Download da página de registro de ponto. Extrai o texto do HTML da página. """
         try:
             url = self.opener.open(self.urlSisCop)
             url = self.opener.open(self.urlSisCop, urllib.urlencode(self.fields))
@@ -128,57 +105,64 @@ class SisCopService(Service):
         parser.close()
         if start < 0 or end < 0:
             raise Exception(u'SisCop - The page layout is unknown')
+        return parser.texto
 
-        #Extraí a data da última saída
-        text = parser.texto
-        dtSaida = self.extractDate(text, 1, entrance=False)
-        if dtSaida != None:
-            self.releaseTimerExit1Period()
-            print u"Exit date first period: ", dtSaida
-            if self.hasEntrada(text):
-                self.terminated = True #Está tudo OK
-            else:
-                #Verifica se a diferença é menor que uma hora
-                diff = datetime.datetime.today() - dtSaida
-                self.horaRetorno = dtSaida + datetime.timedelta(seconds=SEC_HOUR)
-                if diff.seconds < SEC_HOUR: #menor que uma hora
-                    secDiff = SEC_HOUR - diff.seconds
-                    if self.timer == None:
-                        self.timer = Timer(secDiff, self.onTimer)
-                        self.timer.setDaemon(True)
-                        self.timer.start()
-                        print "timer started"
-                    #executa novamente em 1 minuto após a hora prevista para registro
-                    self.refreshMinutes = 1.0 + float(secDiff)/60.0
-                else:
-                    self.refreshMinutes = 5 # em 5 minutos verifica novamente
-                    self.onTimer()
-                return False
-        elif self.alertExit1Period:
-            dtEntrada = self.extractDate(text, 1, entrance=True)
-            if dtEntrada != None:
-                #Verifica se a diferença é menor que 5 horas
-                diff = datetime.datetime.today() - dtEntrada
-                if diff.seconds < SEC_MAX_PERIOD: #menor que 5 horas
-                    if self.timerExit1Period == None:
-                        secDiff = SEC_MAX_PERIOD - diff.seconds + 5 # mais 5 segundos pra garantir que vai entrar no else
-                        timer = Timer(secDiff, self.check)
-                        timer.setDaemon(True)
-                        timer.start()
-                        self.timerExit1Period = timer
-                        print "timer started (to exit)"
-                else:
-                    self.alertExit1Period = False
-                    self.releaseTimerExit1Period()
-                    self.showPage()
-        return True
-                
+    def checkPeriod(self, dtEntr, dtExit):
+        """ Verifica se já se passou mais de 5 horas da entrada do período. """
+        if dtEntr == None or dtExit != None:
+            return True
+        diff = datetime.datetime.today() - dtEntr
+        if diff.seconds < SEC_ALERT_PERIOD: # menor que 5 horas
+            secDiff = SEC_ALERT_PERIOD - diff.seconds + 5 # mais 5 segundos pra garantir que vai entrar no else
+            self.refreshMinutes = min(self.refreshMinutes, float(secDiff)/60.0)
+        elif diff.seconds < SEC_MAX_PERIOD:
+            self.showPage(dtEntr)
+        else:
+            return True
+        return False
 
-    def hasEntrada(self, text):
-        return self.extractDate(text, 2, entrance=True) != None
+    def checkReturn(self, exit1, entr2):
+        """ Verifica se o retorno do almoço foi registrado. """
+        if exit1 == None or entr2 != None:
+            return True
+        diff = datetime.datetime.today() - exit1
+        self.timeReturn = exit1 + datetime.timedelta(seconds=SEC_HOUR)
+        if diff.seconds < SEC_HOUR: # menor que uma hora
+            secDiff = SEC_HOUR - diff.seconds + 1 # 1 segundo a mais
+            self.refreshMinutes = min(self.refreshMinutes, float(secDiff)/60.0)
+        else: # Maior que 1 hora
+            self.refreshMinutes = 2 # em 2 minutos verifica novamente
+            self.showPage(exit1)
+        return False
+
+
+    def check(self):
+        """ Verifica se já está na hora de bater o ponto, observando os horários de saída e o limite máximo de um período. """
+        try:
+            text = self.getPageText()
+        except:
+            self.refreshMinutes = 5 # em caso de erro verifica a página novamente em 5 minutos
+            raise
+
+        #Atualiza a cada 55min
+        self.refreshMinutes = 55
+        self.timeReturn = None
+
+        entr1 = self.extractDate(text, 1, entrance=True)
+        exit1 = self.extractDate(text, 1, entrance=False)
+        entr2 = self.extractDate(text, 2, entrance=True)
+        exit2 = self.extractDate(text, 2, entrance=False)
+
+        if self.checkPeriod(entr1, exit1) and self.checkReturn(exit1, entr2):
+            self.checkPeriod(entr2, exit2)
+
+        return (self.timeReturn == None)
 
     def extractDate(self, text, period, entrance):
-        """ Extraí a data de entrada ou saída no período informado """
+        """ Extraí a data de entrada ou saída no período informado 
+        Formato:
+        1º Período
+        Saída- 11:58 """
         periodFound = False
         strPeriod = str(period) + u'º Período'
         if entrance:
@@ -207,3 +191,13 @@ class SisCopService(Service):
         #se chegar até aqui, é porque o layout é desconhecido
         raise Exception(u'SisCop - The page layout is unknown')
 
+########################################
+# main
+
+if __name__ == '__main__':
+    from monitors import MonApp, MonLoginWindow
+    app = MonApp()
+    app.name = 'SisCop Checker'
+    app.addService(SisCopService)
+    MonLoginWindow(app).run()
+    app.run()
