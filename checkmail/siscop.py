91c1aaabad5299166b91c619dcac1b1fb36cbbee
Israel Rios <isrrios@gmail.com>
2018-08-01 18:14:57 -0300

    Supporting siscop's new version. Disabliing auto-login.

--- checkmail/siscop.py
+++ checkmail/siscop.py
@@ -3,29 +3,23 @@
 # Autor: Israel Rios
 # Created: 15-abr-2009
 
-from monitors import Service, TrayIcon, curdir, showMessage
-from monutil import HtmlTextParser, execute, decode_htmlentities, getProwl
+from monitors import Service, TrayIcon, curdir
+from monutil import execute, getProwl
 
 import os
-import urllib
-import urllib2
-import cookielib
+import requests
 import datetime
 import dbus
 import commands
 import threading
-import gtk
-import re
-import tempfile
 import sys
-import subprocess
+import simplejson
 
 SEC_HOUR = 60 * 60  # 1 hora em segundos
 
-SEC_INTERVAL = SEC_HOUR / 2 # intervalo de almoço (1/2 hora)
+SEC_INTERVAL = SEC_HOUR / 2  # intervalo de almoço (1/2 hora)
 
 SEC_MAX_PERIOD = SEC_HOUR * 5  # 5hs
-SEC_ALERT_PERIOD = SEC_MAX_PERIOD - (60 * 2)  # 4hs e 58mins
 
 NOT_LOGGED = 3
 PONTO_OK = 1
@@ -38,11 +32,6 @@ class SisCopTrayIcon(TrayIcon):
 
     def prepareMenu(self, menu):
         TrayIcon.prepareMenu(self, menu)
-        menu.append(self.createMenuItem(gtk.STOCK_CONNECT, self.onMenuConnect))
-        # menu.append(self.createMenuItem("Leave Alert", self.onMenuConnect))
-
-    def onMenuConnect(self, event):
-        self.service.decodeCaptcha()
 
     def setIcon(self, status):
         iconname = None
@@ -69,33 +58,37 @@ class NotLoggedException(Exception):
 
 
 class SisCopService(Service):
-    urlSisCop = 'https://siscop.portalcorporativo.serpro'
-    urlLogin = urlSisCop + '/cpf_senha.asp'
-    urlCadRegPonto = urlSisCop + '/CadRegPonto.asp'
+    urlSisCop = 'https://siscopweb.serpro'
+    urlLoginPage = urlSisCop + '/login.html'
+    urlCadRegPonto = urlSisCop + '/registro.html'
+    urlAuth = urlSisCop + '/api/auth'
+    urlRegistro = urlSisCop + '/api/registro'
 
     def __init__(self, app, user, passwd):
         Service.__init__(self, app, user, passwd)
         # Os campos do formulário
-        self.fields = {}
-        self.fields['tx_cpf'] = user
-        self.fields['tx_senha'] = passwd
+        self.fields = {'username': user, 'password': passwd}
         # Inicialização
-        self.opener, self.cookiejar = self.buildOpener()
-        self.tempOpener = None
-        self.tempCookiejar = None
-        # Atualiza a cada 5min
-        self.refreshMinutes = 5
+        self.token = None
+        self.cookiejar = requests.cookies.RequestsCookieJar()
+        # Atualiza a cada 29min
+        self.refreshMinutes = 29
         self.lastPageId = None
         self.lastProwl = None
         self.logged = False
-        self.captchaValue = None
+        self.timeReturn = None
         self.prowl = getProwl()
 
-    def buildOpener(self):
-        cookiejar = cookielib.CookieJar()  # cookies são necessários para a autenticação
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookiejar))
-        opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux i686; rv:35.0) Gecko/20100101 Firefox/35.0')]
-        return (opener, cookiejar)
+    def getHeaders(self):
+        headers = {
+            'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36'
+                          ' (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Origin': self.urlSisCop,
+            'Accept': '*/*'}
+        if self.token:
+            headers['Authorization'] = 'Token ' + self.token
+        return headers
 
     def createTrayIcon(self):
         return SisCopTrayIcon(self)
@@ -117,6 +110,7 @@ class SisCopService(Service):
         if self.prowl is None:
             return
 
+        # noinspection PyBroadException
         try:
             self.prowl.post("Siscop", "", msg)
         except:
@@ -130,7 +124,7 @@ class SisCopService(Service):
             if pageId is not None:
                 self.lastPageId = pageId
             # verifica se a sessão ainda é válida, senão faz login.
-            self.checkLogged(self.openUrlRegPonto())
+            self.checkLogged(self.requestRegistro())
             if not self.logged:
                 self.login()
             # abre o browser com a página
@@ -149,6 +143,7 @@ class SisCopService(Service):
 
         # verifica se o usuário está na máquina
         if pageId is not None:
+            # noinspection PyBroadException
             try:
                 bus = dbus.SessionBus()
                 ssaver = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
@@ -158,211 +153,147 @@ class SisCopService(Service):
                 pass  # costuma lançar um erro DBusException: org.freedesktop.DBus.Error.NoReply
 
     def buildUrlRegPonto(self):
-        if self.logged:
-            # passa os cookies para o browser para evitar a tela de login
-            url = self.urlCadRegPonto + "?"
-            cookies = []
-            for cookie in self.cookiejar:
-                cookies.append("%s=%s" % (cookie.name, cookie.value))
-            url = url + urllib.urlencode({'cookie': '; '.join(cookies)})
-        else:
-            url = self.urlLogin
-        return url
+        return self.urlLoginPage
+        # passa os cookies para o browser para evitar a tela de login
+        # url = self.urlCadRegPonto + "?"
+        # cookies = []
+        # for cookie in self.cookiejar:
+        #     cookies.append("%s=%s" % (cookie.name, cookie.value))
+        # url = url + urllib.urlencode({'cookie': '; '.join(cookies), 'token': self.token})
 
-    def decodeCaptcha(self):
-        self.captchaValue = None
-        for i in range(10):
-            decoded = self.tryDecodeCaptcha()
-            if decoded is not None:
-                self.captchaValue = decoded
-                break
-
-    def tryDecodeCaptcha(self):
-        self.tempOpener, self.tempCookiejar = self.buildOpener()
-        html = self.tempOpener.open(self.urlLogin).read()
-        # get the viewstate value
-        mo = re.search(r"<input[^>]+id\s*=\s*['\"]?viewstate['\"]?[^>]+value\s*=\s*'([^']+)'", html, re.DOTALL)
-        self.fields['viewstate'] = decode_htmlentities(mo.group(1))
-        # get the captcha image url
-        mo = re.search(r"src\s*=\s*'(/captcha/[^']+)'", html, re.DOTALL)
-        fname = os.path.join(tempfile.gettempdir(), "siscop{0}.captcha.jpg".format(self.user))
-        with open(fname, 'wb') as f:
-            f.write(self.tempOpener.open(self.urlSisCop + decode_htmlentities(mo.group(1))).read())
-        decoded = subprocess.check_output(['java', '-jar', '/opt/capres/capres.jar', fname])
-        decoded = decoded.strip()
-        if len(decoded) != 5:
-            return None
-        return decoded
-
-    def checkLogged(self, url):
-        self.logged = not url.geturl().startswith(self.urlLogin)
+    def checkLogged(self, resp):
+        self.logged = self.token and resp.status_code != 401 and not resp.url.startswith(self.urlLoginPage)
         return self.logged
 
-    def saveCookies(self):
-        cookiesFileName = os.path.join(
+    def saveToken(self):
+        tokenFileName = os.path.join(
             os.getenv('USERPROFILE') or os.getenv('HOME') or os.path.abspath(os.path.dirname(sys.argv[0])),
-            '.siscop_cookies')
-        with open(cookiesFileName, 'w') as f:
-            for cookie in self.cookiejar:
-                f.write("%s=%s\n" % (cookie.name, cookie.value))
+            '.siscop_token')
+        with open(tokenFileName, 'w') as f:
+            f.write("%s\n" % self.token)
 
     def login(self):
         if self.logged:
             return True
-        for i in range(5):
-            self.decodeCaptcha()
-            if self.captchaValue is None:
-                return False
-            url = self.tryLogin()
-            if 'Sequência de caracteres não confere' in url.read().decode('cp1252'):
-                continue
-            if self.checkLogged(url):
-                self.saveCookies()
-            break
+        url = self.requestLogin()
+        if self.checkLogged(url):
+            self.saveToken()
         return self.logged
 
-    def tryLogin(self):
-        self.opener = self.tempOpener
-        self.cookiejar = self.tempCookiejar
-        self.fields['captcha'] = self.captchaValue
-        self.captchaValue = None
-        self.tempOpener = None
-        self.tempCookiejar = None
-        return self.opener.open(self.urlLogin, urllib.urlencode(self.fields))
+    def requestLogin(self):
+        requests.get(self.urlLoginPage, cookies=self.cookiejar)
+        self.token = None
+        resp = requests.post(self.urlAuth, json=self.fields,
+                             headers=self.getHeaders(), cookies=self.cookiejar)
+        self.token = resp.headers.get('Set-Token', None)
+        return resp
 
-    def openUrlRegPonto(self):
+    def requestRegistro(self):
         try:
-            return self.opener.open(self.urlCadRegPonto)
+            return requests.get(self.urlRegistro, headers=self.getHeaders(), cookies=self.cookiejar)
         except Exception, e:
             raise Exception(u"It was not possible to connect at SisCop. Error:\n\n" + str(e))
 
-    def getPageText(self):
-        """ Faz o login no SISCOP. Download da página de registro de ponto. Extrai o texto do HTML da página. """
-        url = self.openUrlRegPonto()
+    def getRegistro(self):
+        """ Obtêm o último registro de ponto. Formato:
+        {
+            "tipo" : "E",
+            "hora" : "2018-08-01 09:48:00",
+            "registroAutomatico" : true,
+            "dataBase" : "2018-08-01 09:48:00",
+            "referenteJornadaDiaAnterior" : false,
+            "registradoPor" : "11111111111",
+            "ipRegistro" : "10.32.129.29"
+        }
+        """
+        resp = self.requestRegistro()
 
-        self.checkLogged(url)
-        if not self.logged:
+        if not self.checkLogged(resp):
             raise NotLoggedException()
 
-        # Extraí as linhas de interese e remove as tags HTML
-        start = end = -1
-        parser = HtmlTextParser()
-        for line in url.readlines():
-            line = line.decode('cp1252')
-            if start < 0:
-                # ascentos não estão funcionando, dá problema de codificação
-                start = line.find(u'Período Normal')  # Período Normal
-            if start >= 0:
-                if end < 0:
-                    end = line.find(u'Período Extra')  # Período Extra
-                    parser.feed(line)
+        registros = simplejson.loads(resp.text)
+        if len(registros) == 0:
+            return None
 
-        parser.close()
-        if start < 0 or end < 0:
-            raise Exception(u'SisCop - The page layout is unknown')
-        return parser.texto
+        # todos os registros do dia são obtidos, pegamos só o último
+        registro = registros[-1]
+        registro['hora'] = SisCopService.toDate(registro['hora'])
+        return registro
 
-    def checkPeriod(self, dtEntr, dtExit):
+    def checkPeriod(self, registro):
         """ Verifica se já se passou mais de 5 horas da entrada do período. """
-        if dtEntr is None or dtExit is not None:
+        if registro is None or registro['tipo'] != 'E':
             return True
-        diff = datetime.datetime.today() - dtEntr
-        if diff.seconds < SEC_ALERT_PERIOD:  # menor que 5 horas
-            secDiff = SEC_ALERT_PERIOD - diff.seconds + 5  # mais 5 segundos pra garantir que vai entrar no else
+        diff = datetime.datetime.today() - registro['hora']
+        if diff.seconds < SEC_MAX_PERIOD:  # menor que 5 horas
+            secDiff = SEC_MAX_PERIOD - diff.seconds + 1  # mais 1 segundo pra garantir que vai entrar no else
             self.refreshMinutes = min(self.refreshMinutes, float(secDiff) / 60.0)
-        elif diff.seconds < SEC_MAX_PERIOD:
-            self.showPage(dtEntr)
-        else:
-            return True
+        else:  # maior que 5 horas
+            self.showPage(registro['hora'])
         return False
 
-    def checkReturn(self, exit1, entr2):
+    def checkReturn(self, registro):
         """ Verifica se o retorno do almoço foi registrado. """
-        if exit1 is None or entr2 is not None:
+        if registro is None or registro['tipo'] == 'E' or registro['hora'].hour >= 15:
             return True
-        diff = datetime.datetime.today() - exit1
-        self.timeReturn = exit1 + datetime.timedelta(seconds=SEC_INTERVAL)
+        diff = datetime.datetime.today() - registro['hora']
+        self.timeReturn = registro['hora'] + datetime.timedelta(seconds=SEC_INTERVAL)
         if diff.seconds < SEC_INTERVAL:  # menor que o intervalor mínimo (1/2 hora)
             secDiff = SEC_INTERVAL - diff.seconds + 1  # 1 segundo a mais
             self.refreshMinutes = min(self.refreshMinutes, float(secDiff) / 60.0)
-        else:  # Maior que 1 hora
+        else:  # Maior que o intervalo mínimo
             self.refreshMinutes = 2  # em 2 minutos verifica novamente
-            self.showPage(exit1)
+            self.showPage(registro['hora'])
         return False
 
     def check(self):
-        """ Verifica se já está na hora de bater o ponto, observando os horários de saída e o limite máximo de um período. """
+        """ Verifica se já está na hora de bater o ponto, observando os horários de saída e o limite máximo de um
+        período. """
         # Atualiza a cada 29min
         self.refreshMinutes = 29
         if not self.login():
             return NOT_LOGGED
         try:
-            text = self.getPageText()
+            registro = self.getRegistro()
         except NotLoggedException:
             if not self.login():
                 return NOT_LOGGED
             try:
-                text = self.getPageText()
+                registro = self.getRegistro()
             except NotLoggedException:
                 return NOT_LOGGED
 
         self.timeReturn = None
 
-        entr1 = self.extractDate(text, 1, entrance=True)
-        exit1 = self.extractDate(text, 1, entrance=False)
-        entr2 = self.extractDate(text, 2, entrance=True)
-        exit2 = self.extractDate(text, 2, entrance=False)
-
-        if self.checkPeriod(entr1, exit1) and self.checkReturn(exit1, entr2):
-            self.checkPeriod(entr2, exit2)
+        if self.checkPeriod(registro):
+            self.checkReturn(registro)
 
         if self.timeReturn is None:
             return PONTO_OK
         return PONTO_NOK
 
-    def extractDate(self, text, period, entrance):
-        """ Extraí a data de entrada ou saída no período informado
-        Formato:
-        1º Período
-        Saída- 11:58 """
-        periodFound = False
-        strPeriod = str(period) + u'º Período'
-        if entrance:
-            desc = u'Entrada'
-        else:
-            desc = u'Saída'
-        for line in text.split('\n'):
-            line = line.strip()
-            if line != '':
-                if line.startswith(strPeriod):
-                    periodFound = True
-                if periodFound and line.startswith(desc):
-                    try:
-                        parts = line.split('-')
-                        if len(parts) < 2:
-                            # Sem horário
-                            return None
-                        else:
-                            shour = parts[1].strip().split(':')
-                            today = datetime.date.today()
-                            return datetime.datetime(year=today.year, month=today.month,
-                                                     day=today.day, hour=int(shour[0]),
-                                                     minute=int(shour[1]))
-                    except:
-                        raise Exception(u'SisCop - The page layout is unknown')
-        # se chegar até aqui, é porque o layout é desconhecido
-        raise Exception(u'SisCop - The page layout is unknown')
+    @staticmethod
+    def toDate(text):
+        """ Converte para data. Formatado da entrada: 2018-08-01 09:48:00"""
+        try:
+            return datetime.datetime.strptime(text, "%Y-%m-%d %H:%M:%S")
+        except:
+            raise Exception(u'SisCop - The page layout is unknown')
 
 
 ########################################
 # main
 
-if __name__ == '__main__':
-    from monitors import MonApp, MonLoginWindow
-
+def run():
     app = MonApp()
     app.appid = 'scc'
     app.name = 'SisCop Checker'
     app.addService(SisCopService)
     MonLoginWindow(app).run()
     app.run()
+
+
+if __name__ == '__main__':
+    from monitors import MonApp, MonLoginWindow
+
+    run()
